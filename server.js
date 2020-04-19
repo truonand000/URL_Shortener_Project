ad989665b5d2663b3d5339cd9aaf02da7e5b1ada
Andrew Truong <truong.an@husky.neu.edu>
2020-04-19 09:02:01 -0700

    Set up app to run both client and server

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// Static file declaration
+app.use(express.static(path.join(__dirname, 'client_url_shortener/build')));
+
+// Production mode
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client_url_shortener/build')));
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'client_url_shortener/build/index.html'));
+    })
+}
+
+// Build mode
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client_url_shortener/public/index.html'));
+})
 
 app.listen(port, () => {
     console.log(`Starting server on port ${port}`);
