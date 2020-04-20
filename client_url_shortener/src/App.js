cca49693e0f92e27dad7e335a84b7c8348fbe856
Andrew Truong <truong.an@husky.neu.edu>
2020-04-19 22:13:31 -0700

    Began frontend for the app

--- client_url_shortener/src/App.js
+++ client_url_shortener/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Col, Container, Row, Form, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+          <Row>
+              <Col className="text-center">
+                  <h1>
+                      URL Shortener
+                  </h1>
+                  <Form>
+                      <Form.Group controlId="formBasicInput">
+                          <Form.Label>URL to be Shortened</Form.Label>
+                          <Form.Control type="email" placeholder="Input URL" />
+                      </Form.Group>
+
+                      <Form.Group controlId="formBasicOutput">
+                          <Form.Label>Output URL</Form.Label>
+                          <Form.Control type="email" placeholder="Output URL" />
+                      </Form.Group>
+                      <Button variant="primary" type="submit">
+                          Shorten
+                      </Button>
+                  </Form>
+              </Col>
+          </Row>
+      </Container>
   );
 }
 
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const mongoose = require('mongoose');
+
+const mongoEndpoint = process.env.MONGODB_URI || 'mongodb://127.0.0.1/url_shortener_app';
+mongoose.connect(mongoEndpoint, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,8 +29,8 @@ if (process.env.NODE_ENV === 'production') {
 // Build mode
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client_url_shortener/public/index.html'));
-})
+});
 
 app.listen(port, () => {
     console.log(`Starting server on port ${port}`);
-})
+});
